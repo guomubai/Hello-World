d8aa23d1dd20d166fe8f84fb090c5d6131465f63
Guomubai <erickjgarcia@gmail.com>
2019-03-11 20:06:18 -0600

    Initial commit

---
+++ .idea/src/Car.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+
+public class Car {
+
+    //Datatypes
+    //int -> interger 1,2,3
+    // double -> decimal 34.5
+    //String - you know this
+    //Color -> from awt library "abstract window tool"
+    // boolean -> true or false of course
+
+    double averageMilesPerGallon;
+    String licencePlate;
+    Color paintColor;
+    boolean areTailLightsWorking;
+
+    public Car (double inputaAverageMPG,
+                String inputLicensePlate,
+                Color inputPaintColor,
+                boolean inputAreTailLightsWorking) {
+        this.averageMilesPerGallon = inputaAverageMPG;
+        this.licencePlate = inputLicensePlate;
+        this.paintColor = inputPaintColor;
+        this.areTailLightsWorking = inputAreTailLightsWorking;
+    }
+}
---
+++ .idea/src/Main.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+public class Main {
+
+    public static void main  (String[] args) {
+        System.out.println("Hello World! My name is Erick");
+        System.out.println("Hi!");
+
+        Car myCar = new Car(25.5,
+                "1BCERICK", Color.BLUE, true);
+
+        Car sallyCar = new Car(30.9, "lolwhatm8", Color.BLACK, false);
+
+        System.out.println("My car License Plate: " + myCar.licencePlate);
+        System.out.println("Sally's License Plate: " + sallyCar.licencePlate);
+    }
+
+
+
+
+}
